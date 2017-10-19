41f2fc71dd5bd266a379a78288819464d7612b9f
Mike Talbo <mike.talbo@nationalnet.com>
2017-10-19 12:30:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# vhostparser
+## Description
+`vhostparser` is a vhost parser.
+
+## Installation
+Just put it in `/usr/local/bin` and `chmod +x` it.
+
+## Invokation
+You just feed it a file.  That's all.  It will only take one at a time,
+so if you have multiple files, you'll have to loop it.
+
+```
+vhostparser [INFILE]
+```
+
+## Contact
+If you have questions about this script, or getting it set up, all of my
+contact information is valid per company standards for email, slack, and
+phone. 
---
+++ vhostparser.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+file_config="${1}";
+file_config_short="${file_config}_short";
+
+function reset_variables() {
+  host_ips="";
+  host_name="";
+  host_aliases="";
+  out_line="";
+}
+
+function unset_variables() {
+  unset host_ips;
+  unset host_name;
+  unset host_aliases;
+  unset out_line;
+}
+
+reset_variables;
+
+grep -iP '(VirtualHost|Server(Name|Alias)|DocumentRoot)' ${file_config} > ${file_config_short};
+
+cat ${file_config_short} | grep -vP '^( |       )+?$' | while read line; do
+  if [[ ${line} =~ \<VirtualHost\ .*\> ]]; then
+    host_ips=$(echo ${line} | perl -pe 's#(<VirtualHost |>)##g' | perl -pe 's# #,#g');
+  elif [[ ${line} =~ ServerName\ .* ]]; then
+    host_name=$(echo ${line} | perl -pe 's#ServerName ##g');
+  elif [[ ${line} =~ ServerAlias\ .* ]]; then
+    if [[ ${host_aliases} == "" ]]; then
+      host_aliases=$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's# #,#g');
+    else
+      host_aliases+=",$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's# #,#g')";
+    fi
+  elif [[ ${line} =~ DocumentRoot\ .* ]]; then
+    host_root=$(echo ${line} | perl -pe 's#DocumentRoot ##g');
+  elif [[ ${line} =~ \</VirtualHost\> ]]; then
+    out_line="${host_ips} ${host_name} ${host_aliases} ${host_root}";
+    echo ${out_line};
+    reset_variables;
+  else
+    echo "vhostparser: caught bad line: '${line}'"
+    unset_variables;
+    exit 1;
+  fi;
+done
