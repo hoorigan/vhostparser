70ce0b6092bcefdd6802a14ba73bce4787ddf5c9
Mike Talbo <shillelagh.law.512@gmail.com>
2018-02-22 04:07:39 -0500

    Output a CSV instead of the dumb shit I was doing.

--- vhostparser.sh
+++ vhostparser.sh
@@ -23,19 +23,19 @@ grep -iP '(VirtualHost|Server(Name|Alias)|DocumentRoot)' ${file_config} | grep -
 
 cat ${file_config_short} | grep -vP '^( |       )+?$' | while read line; do
   if [[ ${line} =~ \<VirtualHost\ .*\> ]]; then
-    host_ips=$(echo ${line} | perl -pe 's#(<VirtualHost |>)##g' | perl -pe 's# #,#g');
+    host_ips=$(echo ${line} | perl -pe 's#(<VirtualHost |>)##g' | perl -pe 's#,# #g');
   elif [[ ${line} =~ ServerName\ .* ]]; then
     host_name=$(echo ${line} | perl -pe 's#ServerName ##g');
   elif [[ ${line} =~ ServerAlias\ .* ]]; then
     if [[ ${host_aliases} == "" ]]; then
-      host_aliases=$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's# #,#g');
+      host_aliases=$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's#,# #g');
     else
-      host_aliases+=",$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's# #,#g')";
+      host_aliases+=",$(echo ${line} | perl -pe 's#ServerAlias ##g' | perl -pe 's#,# #g')";
     fi
   elif [[ ${line} =~ DocumentRoot\ .* ]]; then
     host_root=$(echo ${line} | perl -pe 's#DocumentRoot ##g');
   elif [[ ${line} =~ \</VirtualHost\> ]]; then
-    out_line="${host_ips} ${host_name} ${host_aliases} ${host_root}";
+    out_line="${host_ips},${host_name},${host_aliases},${host_root}";
     echo ${out_line};
     reset_variables;
   else
